dd8e67ff78c7181bc4fce30ff15b4375bb4dc6d2
Alexey.Mitkin <alexey.mitkin@forextime.com>
2020-04-21 13:54:24 +0300

    Complete two exam tasks

---
+++ Assets/Scripts/Cube_Animation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Animation : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetInteger("Cube_animate", 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetInteger("Cube_animate", 1);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetInteger("Cube_animate", 2);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetInteger("Cube_animate", 3);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            animator.SetInteger("Cube_animate", 4);
+
+        }
+    }
+}
